4df67ed7ca9104f87a1a6eb459f8da71a29e7357
이종호 <pic6367@naver.com>
2020-11-05 11:32:33 +0900

    S03P31D203-70 | roomdao 수정 | 이종호

--- MapmO/app/src/main/java/com/example/mapmo/MemoRoomDao.kt
+++ MapmO/app/src/main/java/com/example/mapmo/MemoRoomDao.kt
@@ -1,17 +1,22 @@
 package com.example.mapmo
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
+import androidx.room.*
 import androidx.room.OnConflictStrategy.REPLACE
-import androidx.room.Query
 
 @Dao
 interface MemoRoomDao {
     @Query("select * from orm_memo")
     fun getAll(): List<MemoRoom>
+    @Query("select * from orm_memo WHERE alarmdate = :stdDate")
+    fun getAllInDay(stdDate: String): List<MemoRoom>
+    @Query("SELECT * FROM orm_memo WHERE alarmdate BETWEEN :startDate AND :endDate")
+    fun getAllBetweenWeek(startDate: String, endDate: String): List<MemoRoom>
+    @Query("SELECT * FROM orm_memo WHERE alarmdate BETWEEN :startDate AND :endDate")
+    fun getAllBetweenMonth(startDate: String, endDate: String): List<MemoRoom>
     @Insert(onConflict = REPLACE)
     fun insert(memo: MemoRoom)
     @Delete
     fun delete(memo: MemoRoom)
+    @Update
+    fun update(memo: MemoRoom)
 }
